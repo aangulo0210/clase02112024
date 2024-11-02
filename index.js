0bb9efa1c4226331b3fcd5c9262e1ec979723332
aangulo0210 <andres.alexander0210@gmail.com>
2024-11-02 09:05:45 -0500

    inicio de clase

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("hola technalia");
+console.log("hola technalia"); // Mensaje por consola
+let numero = 2; // Variable que va a cambiar
+const numero3 = 3; // Variable constante
+let boleano = true; // Variable Boleana, falso o verdadero
+let null = null; // Valor por defecto, definido por programación
+let NoDefinido = undefined; // Dado por el sistema
